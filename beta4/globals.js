7e06406a810e6b4f72ef7def2f3fa700e0f19cdd
Antonio Dal Borgo <adalborgo@gmail.com>
2021-07-24 17:20:26 +0200

    Update globals.js

--- beta4/globals.js
+++ beta4/globals.js
@@ -1,7 +1,7 @@
 /**
  * @package: UnoBlockly
  * @file globals.js
- * @version 0.1 (23-07-2021)
+ * @version 0.1 (24-07-2021)
  * @note Global variables for the package
  * @author Antonio Dal Borgo <adalborgo@gmail.com>
  */
@@ -9,7 +9,7 @@
 const APP_NAME = 'UnoBlockly'; // Important!!!
 const APP_VERSION = '(Beta0.4.0)';
 const EXTENSION = "ubk";
-const LATEST_URL = "https://github.com/adalborgo/UnoBlockly/blob/beta4/beta4/latest.js";
+const LATEST_URL = "https://github.com/adalborgo/UnoBlockly/blob/beta4/latest.js";
 
 var code_editor = false;
 var fileToLoad = "";
