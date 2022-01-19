a4bb518f66b20f02711a1e4b4503d44f0aa8efc8
Antonio Dal Borgo <adalborgo@gmail.com>
2022-01-19 11:54:10 +0100

    Update globals.js

--- beta4/globals.js
+++ beta4/globals.js
@@ -1,13 +1,13 @@
 /**
  * @package: UnoBlockly
  * @file globals.js
- * @version 0.1 (16-11-2021)
+ * @version 0.1 (18-01-2022)
  * @note Global variables for the package
  * @author Antonio Dal Borgo <adalborgo@gmail.com>
  */
 
 const APP_NAME = 'UnoBlockly'; // Important!!!
-const APP_VERSION = '(Beta0.4.3)';
+const APP_VERSION = '(Beta0.4.4)';
 const EXTENSION = "ubk";
 const LATEST_URL = "https://www.dibis.it/UnoBlockly/latest.js";
 
